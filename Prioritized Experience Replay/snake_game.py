13bd9f5d19470ea5573dc6a5219044e37f92d4ce
theolavandier <120734251+Hisqkq@users.noreply.github.com>
2024-08-03 00:39:48 +0200

    Model created

---
+++ Prioritized Experience Replay/snake_game.py
@@ -0,0 +1,126 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple("Point", "x, y")
+
+pygame.display.init()
+pygame.font.init()
+
+font = pygame.font.Font(pygame.font.get_default_font(), 25)
+
+WHITE = (255, 255, 255)
+RED = (200, 0, 0)
+GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+BLACK = (0, 0, 0)
+
+BLOCK_SIZE = 20
+SPEED = 120
+
+class SnakeGameAI:
+    def __init__(self, width=640, height=480):
+        self.w = width
+        self.h = height
+
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption("Snake AI")
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    def reset(self):
+        self.direction = Direction.RIGHT
+        self.head = Point(self.w / 2, self.h / 2)
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
+    def _place_food(self):
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+
+    def play_step(self, action):
+        self.frame_iteration += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        self._move(action)
+        self.snake.insert(0, self.head)
+
+        reward = 0
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
+            reward = -10
+            return reward, True, self.score
+
+        if self.head == self.food:
+            self.score += 1
+            reward = 10
+            self._place_food()
+        else:
+            self.snake.pop()
+
+        self._update_ui()
+        self.clock.tick(SPEED)
+        return reward, False, self.score
+
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        if pt in self.snake[1:]:
+            return True
+        return False
+
+    def _move(self, action):
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clock_wise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[idx]
+        elif np.array_equal(action, [0, 1, 0]):
+            new_dir = clock_wise[(idx + 1) % 4]
+        else:
+            new_dir = clock_wise[(idx - 1) % 4]
+
+        self.direction = new_dir
+
+        x = self.head.x
+        y = self.head.y
+        if self.direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif self.direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif self.direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif self.direction == Direction.UP:
+            y -= BLOCK_SIZE
+
+        self.head = Point(x, y)
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        for pt in self.snake[1:]:
+            pygame.draw.rect(self.display, GREEN, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, ORANGE, pygame.Rect(self.head.x, self.head.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
